348d98a096349cc448ee9eb945031f722f639116
chtnym9 <46001824+chtnym9@users.noreply.github.com>
2020-04-19 14:35:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,3 +26,12 @@ wss.on("connection", function(ws) {
     clearInterval(id)
   })
 })
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
