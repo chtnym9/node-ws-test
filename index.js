f8616f969d2e2106d65afb5ce5bdfbe3fd791135
chtnym9 <46001824+chtnym9@users.noreply.github.com>
2020-04-19 17:56:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,9 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+//wss.on("connection", function(ws) {
+  wss.on('connection', function(ws) {
+    wss.listen('message', function(msg) {
+      wss.send('message', msg);
+    });
+  });
