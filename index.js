30e6d95b58e54709b00dd3ab19802f9c9463aee5
chtnym9 <46001824+chtnym9@users.noreply.github.com>
2020-04-19 18:38:24 +0530

    make it better

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/"))
+//app.use(express.static(__dirname + "/"))
 
-var server = http.createServer(app)
-server.listen(port)
+//var server = http.createServer(app)
+//server.listen(port)
 
-console.log("http server listening on %d", port)
+//console.log("http server listening on %d", port)
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-//wss.on("connection", function(ws) {
-  wss.on('connection', function(ws) {
-    wss.listen('message', function(msg) {
-      wss.send('message', msg);
+//var wss = new WebSocketServer({server: server})
+//console.log("websocket server created")
+//
+//ws.on("connection", function(ws) {
+  ws.on('connection', function(ws) {
+    ws.listen('message', function(msg) {
+      ws.send('message', msg);
+    });
+    ws.on('close',function(){
+      console.log("connection closed")
     });
   });
